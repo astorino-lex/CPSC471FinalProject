394f91af6623b5da024f2ed5d32caf6040689a4a
Dylan-Gordon <dylan.gordon.10@gmail.com>
2018-11-15 13:35:23 -0700

    Updated admin_notifications - DG

--- admin_notifications.php
+++ admin_notifications.php
@@ -1,1 +1,39 @@
-// admin_notifications
+<html>
+	<head>
+		<title>
+			Notifications
+		</title>
+	</head>
+	
+	<?php
+	$admin_name = $_SESSION['adminname'];
+	$sql = "Select * from admin_notifications where email = '".$admin_name."'";
+	
+	$servername = "localhost";
+		$databasename = "cpsc471Project";
+		$username = "dylan";
+		$password = "password";
+		
+		$conn = new mysqli($servername, $username, $password, $databasename);
+		
+	$query = $conn->query($sql);
+	
+	if($query->num_rows > 0)
+	{
+		echo "Notifications:";
+		print "<br>";
+		while($row = $query->fetch_assoc())
+		{
+			echo "Subject: ";
+			echo $row['subject'];
+			print "<br>";
+			echo "Message: ";
+			echo $row['message'];
+		}
+	}
+	
+	
+	?>
+	
+	
+</html>
