4364d6076d7594b49f0f45719472bbf9112465d8
Alexa Astorino <alexa.astorino@gmail.com>
2018-11-15 15:12:08 -0700

    login page updated

--- login_page.php
+++ login_page.php
@@ -1,13 +1,21 @@
 <html>
   <head>
-	<title>Password test</title>
+	   <title>KickstartU Login</title>
   <head>
 
-  <body>
-	<FORM ACTION=process_form.php METHOD=POST>
-	   email: <INPUT TYPE=TEXT NAME="realname"><BR>
-	   password: <INPUT TYPE=PASSWORD NAME="mypassword">
-	  <P><INPUT TYPE=SUBMIT VALUE="submit">
-	</FORM>
+  <body style="background-color:crimson;">
+    <p style="text-align:center;padding-top:75px"><image src="logo.png" class="img-responsive" alt="centered image"></p>
+    <form action=process_form.php method=POST
+          style="padding-top:80px;text-align:center;font-family:impact;font-size:120%;color:black;">
+  	   Email: <input type=TEXT name="user_email"
+          style="display:inline-block;vertical-align:middle;border: 1px solid black;padding: 3px 3px;width:15%;"><BR>
+  	   Password: <input type=PASSWORD name="password"
+          style="display:inline-block;vertical-align:middle;border: 1px solid black;padding: 3px 3px;width:10%;"><P>
+  	  <input type=SUBMIT value="Login" style="font-family:impact;font-size:90%;width:8%;"><P>
+
+        <!--BUTTONS-->
+        <input type="button" value="Create Account" onclick="location='create_account.html'"
+              style="font-family:impact;font-size:90%;width:15%;" />
+	   </form>
   </body>
 </html>
