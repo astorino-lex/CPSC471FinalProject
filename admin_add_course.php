7d4abde63de136457a3d312b8ff9c6191db23b78
Alexa Astorino <alexa.astorino@gmail.com>
2018-11-16 12:34:03 -0700

    admin add course formatted

--- admin_add_course.php
+++ admin_add_course.php
@@ -1,22 +1,22 @@
 <html>
-	<body>
-		<button onclick="history.go(-1);">Back To Home Page </button>
-		<br><br>
+<body style="background-color:crimson;">
+	<p style="text-align:right;padding-top:75px;padding-right:50px;"><image src="logo.png" class="img-responsive" alt="centered image"
+				height="100", width="300"></p>
+		<input type="button" value="Back to Home Page" onclick="history.go(-1);"
+				style="margin-left: 80%;font-family:impact;font-size:90%;width:12%;color:black;">
 		<form action=admin_add_course_tmp.php method=POST
 			style="padding-top:80px;text-align:center;font-family:impact;font-size:120%;color:black;">
 			Course Name: <input type=TEXT name="course_name"
-			style="display:inline-block;vertical-align:middle;border: 1px solid black;padding: 3px 3px;width:15%;"><BR><BR>
+			style="display:inline-block;vertical-align:middle;border: 1px solid black;padding: 3px 3px;width:10%;"><P>
 			Course ID: <input type=TEXT name="course_id"
-			style="display:inline-block;vertical-align:middle;border: 1px solid black;padding: 3px 3px;width:10%;"> <BR><BR>
+			style="display:inline-block;vertical-align:middle;border: 1px solid black;padding: 3px 3px;width:10%;"> <P>
 			Title: <input type=TEXT name="course_title"
-			style="display:inline-block;vertical-align:middle;border: 1px solid black;padding: 3px 3px;width:15%;"><BR><BR>
+			style="display:inline-block;vertical-align:middle;border: 1px solid black;padding: 3px 3px;width:15%;"><P>
 			Semester: <input type=TEXT name="course_semester"
-			style="display:inline-block;vertical-align:middle;border: 1px solid black;padding: 3px 3px;width:10%;"><BR><BR>	
-			
+			style="display:inline-block;vertical-align:middle;border: 1px solid black;padding: 3px 3px;width:10%;"><P>
 		<input type=SUBMIT value="Add Course" style="font-family:impact;font-size:90%;width:10%;"><P>
 
-        <!--BUTTONS-->
 	   </form>
-	   
+
 	</body>
 <html>
