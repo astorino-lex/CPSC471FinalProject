b24a2477a7eda0291deda8ee161f7c2f29f6c8ce
Alexa Astorino <alexa.astorino@gmail.com>
2018-11-16 12:58:41 -0700

    added required fields

--- admin_add_course.php
+++ admin_add_course.php
@@ -6,14 +6,18 @@
 				style="margin-left: 80%;font-family:impact;font-size:90%;width:12%;color:black;">
 		<form action=admin_add_course_tmp.php method=POST
 			style="padding-top:80px;text-align:center;font-family:impact;font-size:120%;color:black;">
-			Course Name: <input type=TEXT name="course_name"
-			style="display:inline-block;vertical-align:middle;border: 1px solid black;padding: 3px 3px;width:10%;"><P>
-			Course ID: <input type=TEXT name="course_id"
-			style="display:inline-block;vertical-align:middle;border: 1px solid black;padding: 3px 3px;width:10%;"> <P>
-			Title: <input type=TEXT name="course_title"
-			style="display:inline-block;vertical-align:middle;border: 1px solid black;padding: 3px 3px;width:15%;"><P>
-			Semester: <input type=TEXT name="course_semester"
-			style="display:inline-block;vertical-align:middle;border: 1px solid black;padding: 3px 3px;width:10%;"><P>
+			Course Name: <input type=TEXT name="course_name" minlength ="4" maxlength="4"
+			style="display:inline-block;vertical-align:middle;border: 1px solid black;padding: 3px 3px;width:10%;"
+			required><P>
+			Course ID: <input type=TEXT name="course_id"  minlength ="3" maxlength="3"
+			style="display:inline-block;vertical-align:middle;border: 1px solid black;padding: 3px 3px;width:10%;"
+			required> <P>
+			Title: <input type=TEXT name="course_title"  minlength = "8" maxlength="45"
+			style="display:inline-block;vertical-align:middle;border: 1px solid black;padding: 3px 3px;width:15%;"
+			required><P>
+			Semester: <input type=TEXT name="course_semester"  minlength ="9"
+			style="display:inline-block;vertical-align:middle;border: 1px solid black;padding: 3px 3px;width:10%;"
+			required><P>
 		<input type=SUBMIT value="Add Course" style="font-family:impact;font-size:90%;width:10%;"><P>
 
 	   </form>
--- admin_select_course.php
+++ admin_select_course.php
@@ -48,13 +48,13 @@
 	<form action=admin_course_page.php method=POST
           style="text-align:center;font-family:impact;font-size:120%;color:black;">
   	   Course Name: <input type=TEXT name="course_name"
-          style="display:inline-block;vertical-align:middle;border: 1px solid black;padding: 3px 3px;width:15%;"><BR>
+          style="display:inline-block;vertical-align:middle;border: 1px solid black;padding: 3px 3px;width:15%;"
+					required><BR>
   	   Course ID: <input type=TEXT name="course_id"
-          style="display:inline-block;vertical-align:middle;border: 1px solid black;padding: 3px 3px;width:10%;"><P>
+          style="display:inline-block;vertical-align:middle;border: 1px solid black;padding: 3px 3px;width:10%;"
+					required><P>
   	  <input type=SUBMIT value="Select Course" style="font-family:impact;font-size:90%;width:10%;"><P>
 
-        <!--BUTTONS-->
 	   </form>
-
 
 </html>
