02aee135ebf6142990161eb3555fe73db39d8ca6
Dylan-Gordon <dylan.gordon.10@gmail.com>
2018-11-15 22:21:30 -0700

    added temporary page to process adding a course to the database before 'selecting' the course for the view course page - Dylan

--- admin_add_course.php
+++ admin_add_course.php
@@ -5,7 +5,7 @@
 		</h3>
 		course name, course id, title, semester , admin email, forum id, number of questions, number of answers.
 		
-		<form action=admin_course_page.php method=POST
+		<form action=admin_add_course_tmp.php method=POST
 			style="padding-top:80px;text-align:center;font-family:impact;font-size:120%;color:black;">
 			Course Name: <input type=TEXT name="course_name"
 			style="display:inline-block;vertical-align:middle;border: 1px solid black;padding: 3px 3px;width:15%;"><BR><BR>
