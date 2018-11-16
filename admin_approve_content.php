bc7122cdc2d3ac3616e274c4623868c304db5215
Dylan-Gordon <dylan.gordon.10@gmail.com>
2018-11-16 00:35:15 -0700

    you can now approve and remove content

--- admin_approve_content.php
+++ admin_approve_content.php
@@ -5,4 +5,23 @@ Approve content using mysql script here:
 <br>
 <embed src="Assignment4.pdf" width=800 height=1000	/>
 
+
+<?php 
+
+	$content_title = $_POST['content_title'];
+	$content_id = $_POST['content_id'];
+	
+	$sql = "Update content set approval_status = 'approved' WHERE title = '".$content_title."' AND id=".$content_id.";";
+	
+	$servername = "127.0.0.1";
+	$databasename = "cpsc471project";
+	$username = "dylan";
+	$password = "password";
+
+	$conn = new mysqli($servername, $username, $password, $databasename);
+
+	$conn->query($sql);
+	
+?>
+
 </html>
--- admin_remove_content.php
+++ admin_remove_content.php
@@ -1,5 +1,23 @@
 <html>
 
 	Remove the content from the database here:
+	
+	<?php 
+
+	$content_title = $_POST['content_title'];
+	$content_id = $_POST['content_id'];
+	
+	$sql = "DELETE FROM content WHERE title = '".$content_title."' AND id=".$content_id.";";
+	
+	$servername = "127.0.0.1";
+	$databasename = "cpsc471project";
+	$username = "dylan";
+	$password = "password";
+
+	$conn = new mysqli($servername, $username, $password, $databasename);
+
+	$conn->query($sql);
+	
+?>
 
 </html>
