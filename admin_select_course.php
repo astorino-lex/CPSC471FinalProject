24f62f8941286f448610c4589820b3956cbe72eb
Dylan-Gordon <dylan.gordon.10@gmail.com>
2018-11-16 15:05:58 -0700

    fixed newline problem in select course

--- admin_select_course.php
+++ admin_select_course.php
@@ -39,6 +39,7 @@
 			echo "Course name: ";
 			echo $row['course_name'];
 			echo ", Course id:&nbsp;&nbsp;".$row['id'];
+			print "<BR>";
 			$course_list[$counter] = $row['course_name'];
 			$course_id_list[$counter] = $row['id'];
 			$counter = $counter + 1;
